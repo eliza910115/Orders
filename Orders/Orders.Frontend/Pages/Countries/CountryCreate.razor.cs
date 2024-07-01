7bd0f2ed3cf972a77476a1e0f707c700c995cf9e
Elizabet Blandon <elizabet.blandon@samtel.eu>
2024-07-01 11:58:47 -0500

    CRUD Countries Done

---
+++ Orders/Orders.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,46 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountryCreate
+
+
+    {
+        private Country country = new();
+        private CountryForm? countryForm;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await Repository.PostAsync("/api/countries", country);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
+        }
+
+
+        private void Return()
+        {
+            countryForm!.FormPostedSuccesfully = true;
+            NavigationManager.NavigateTo("/countries");
+        }
+    }
+}
