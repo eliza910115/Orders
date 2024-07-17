902ce69421bf357817cf622b89b6990883d0dc3c
Elizabet Blandon <elizabet.blandon@samtel.eu>
2024-07-16 22:19:47 -0500

    Maestro Generico

--- Orders/Orders.Backend/Controllers/CategoriesController.cs
+++ Orders/Orders.Backend/Controllers/CategoriesController.cs
@@ -4,11 +4,12 @@ using Orders.Shared.Entities;
 
 namespace Orders.Backend.Controllers
 {
+
     [ApiController]
     [Route("api/[controller]")]
-    public class CountriesController : GenericController<Country>
+    public class CategoriesController : GenericController<Category>
     {
-        public CountriesController(IGenericUnitOfWork<Country> unitOfWork) : base(unitOfWork)
+        public CategoriesController(IGenericUnitOfWork<Category> unitOfWork) : base(unitOfWork)
         {
         }
     }
